117fe7b503f5fd57c8d772eb121063499b604e27
guitarbien <guitarbien@gmail.com>
2021-09-29 01:42:00 +0800

    feat: 可透過 named properties 來輕鬆決定實作

--- client.php
+++ client.php
@@ -8,6 +8,18 @@ require 'vendor/autoload.php';
 
 $number = $argv[1];
 
-$generator = new PoemGenerator(new EchoFormatter(), new RandomOrderer());
+$generator = new PoemGenerator();
+
+// $generator = new PoemGenerator(
+//     formatter: new EchoFormatter(),
+// );
+// $generator = new PoemGenerator(
+//     orderer: new RandomOrderer(),
+// );
+// $generator = new PoemGenerator(
+//     formatter: new EchoFormatter(),
+//     orderer: new RandomOrderer(),
+// );
+
 $result = $generator->generate($number);
 echo $result;
