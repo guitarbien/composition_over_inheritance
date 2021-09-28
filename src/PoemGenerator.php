f6d6bf21106e976a6df5dd4f00bba13650626ce3
guitarbien <guitarbien@gmail.com>
2021-09-29 00:51:06 +0800

    feat: random poem generator
    refactor data from property to method

--- src/PoemGenerator.php
+++ src/PoemGenerator.php
@@ -3,20 +3,6 @@ namespace App;
 
 class PoemGenerator
 {
-    private static array $data = [
-        'the horse and the hound and the horn that belonged to', 'the farmer sowing his corn that kept',
-        'the rooster that crowed in the morn that woke',
-        'the priest all shaven and shorn that married',
-        'the man all tattered and torn that kissed',
-        'the maiden all forlorn that milked',
-        'the cow with the crumpled horn that tossed',
-        'the dog that worried',
-        'the cat that killed',
-        'the rat that ate',
-        'the malt that lay in',
-        'the house that Jack built',
-    ];
-
     public function generate(int $number): string
     {
         return "This is {$this->phrase($number)}.";
@@ -24,7 +10,24 @@ class PoemGenerator
 
     protected function phrase(int $number): string
     {
-        $parts = array_slice(self::$data, -$number, $number);
+        $parts = array_slice($this->data(), -$number, $number);
         return implode(PHP_EOL . str_pad(' ', 8), $parts);
+    }
+
+    private function data(): array
+    {
+        return [
+            'the horse and the hound and the horn that belonged to', 'the farmer sowing his corn that kept',
+            'the rooster that crowed in the morn that woke',
+            'the priest all shaven and shorn that married',
+            'the man all tattered and torn that kissed',
+            'the maiden all forlorn that milked',
+            'the cow with the crumpled horn that tossed',
+            'the dog that worried',
+            'the cat that killed',
+            'the rat that ate',
+            'the malt that lay in',
+            'the house that Jack built',
+        ];
     }
 }
