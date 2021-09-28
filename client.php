39b6d52640f206daffb0f37865fb328d1a699843
guitarbien <guitarbien@gmail.com>
2021-09-29 01:35:30 +0800

    feat: 呼叫端注入兩種需要的實作

--- client.php
+++ client.php
@@ -1,11 +1,13 @@
 <?php
 
+use App\EchoFormatter;
 use App\PoemGenerator;
+use App\RandomOrderer;
 
 require 'vendor/autoload.php';
 
 $number = $argv[1];
 
-$generator = new PoemGenerator;
+$generator = new PoemGenerator(new EchoFormatter(), new RandomOrderer());
 $result = $generator->generate($number);
 echo $result;
